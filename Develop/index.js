4414e9e577f5d8e684a90db650bc7f03c43d32c4
km4891 <kmorarity@gmail.com>
2020-10-04 18:18:46 -0700

    first

---
+++ Develop/index.js
@@ -0,0 +1,22 @@
+// array of questions for user
+const questions = [
+    
+    {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?'
+    }
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
